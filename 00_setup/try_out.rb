8ef98f0391e81ce3c87102871556a24be338ae6d
Shu OGAWARA <shu.ogawara@linkers.net>
2020-06-24 16:54:23 +0900

    finished 00_setup

--- 00_setup/try_out.rb
+++ 00_setup/try_out.rb
@@ -1,8 +1,38 @@
 class TryOut
-  # このクラスの仕様
-  # コンストラクタは、2つまたは3つの引数を受け付ける。引数はそれぞれ、ファーストネーム、ミドルネーム、ラストネームの順で、ミドルネームは省略が可能。
-  # full_nameメソッドを持つ。これは、ファーストネーム、ミドルネーム、ラストネームを半角スペース1つで結合した文字列を返す。ただし、ミドルネームが省略されている場合に、ファーストネームとラストネームの間には1つのスペースしか置かない
-  # first_name=メソッドを持つ。これは、引数の内容でファーストネームを書き換える。
-  # upcase_full_nameメソッドを持つ。これは、full_nameメソッドの結果をすべて大文字で返す。このメソッドは副作用を持たない。
-  # upcase_full_name! メソッドを持つ。これは、upcase_full_nameの副作用を持つバージョンで、ファーストネーム、ミドルネーム、ラストネームをすべて大文字に変え、オブジェクトはその状態を記憶する
+  def initialize(first_name, second_name, third_name = nil)
+    if third_name.nil?
+      middle_name = nil
+      last_name = second_name
+    else
+      middle_name = second_name
+      last_name = third_name
+    end
+
+    @first_name = first_name
+    @middle_name = middle_name
+    @last_name = last_name
+  end
+
+  def full_name
+    all_names.compact.join(' ')
+  end
+
+  def first_name=(arg)
+    @first_name = arg
+  end
+
+  def upcase_full_name
+    full_name.upcase
+  end
+
+  def upcase_full_name!
+    all_names.compact.each(&:upcase!)
+    full_name
+  end
+
+  private
+
+  def all_names
+    [@first_name, @middle_name, @last_name]
+  end
 end
