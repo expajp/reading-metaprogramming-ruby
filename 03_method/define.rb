fe4d77c3d8490c91421c4d502029053bad37103d
Shu OGAWARA <shu.ogawara@linkers.net>
2020-07-03 13:58:10 +0900

    ?メソッド以外

--- 03_method/define.rb
+++ 03_method/define.rb
@@ -1,6 +1,11 @@
 # Q1.
 # 次の動作をする A1 class を実装する
 # - "//" を返す "//"メソッドが存在すること
+class A1
+  define_method '//' do 
+    '//' 
+  end
+end
 
 # Q2.
 # 次の動作をする A2 class を実装する
@@ -9,8 +14,36 @@
 # - 2で定義するメソッドは下記とする
 #   - 受け取った引数の回数分、メソッド名を繰り返した文字列を返すこと
 #   - 引数がnilの場合は、dev_teamメソッドを呼ぶこと
+class A2
+  def initialize(arr)
+    arr.each do |val|
+      self.class.class_eval do
+        define_method "hoge_#{val}" do |loop|
+          return dev_team if loop.nil?
+          __method__.to_s*loop
+        end
+      end
+    end
+  end
+
+  def dev_team
+    'SmartHR Dev Team'
+  end
+end
+
 
 # Q3.
 # 次の動作をする OriginalAccessor モジュール を実装する
 # - OriginalAccessorモジュールはincludeされたときのみ、my_attr_accessorメソッドを定義すること
 # - my_attr_accessorはgetter/setterに加えて、boolean値を代入した際のみ真偽値判定を行うaccessorと同名の?メソッドができること
+module OriginalAccessor
+  def self.included(base)
+    base.class_eval do 
+      def self.my_attr_accessor(*args)
+        args.each do |arg|
+					self.class_eval { attr_accessor arg } 
+				end
+      end
+    end
+  end
+end
